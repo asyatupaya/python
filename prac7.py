e66da2e1c0abd0c82da18791322e2802a362b063
rtxdiv <rtxdiv>
2026-04-07 14:00:38 +0300

    upd

--- prac7.py
+++ prac7.py
@@ -8,4 +8,4 @@ SEATS_IN_COMPARTMENT = 4
 
 seat_number = int(input('Введите ваш посадочный номер: '))
 compartment_number = ((seat_number - 1) // SEATS_IN_COMPARTMENT ) + 1
-print(f'Ваше купе: { compartment_number }')
+print(f'Ваше купе: {compartment_number}')
