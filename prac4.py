47195e7409edec5abb9d4742448c962a4aabf7b1
rtxdiv <rtxdiv>
2026-04-07 13:51:52 +0300

    merge projects

---
+++ prac4.py
@@ -0,0 +1,8 @@
+import math
+
+
+(x1, y1) = map(float, input('Введите ккординаты ПЕРВОЙ точки в формате x;y\nКоординаты: ').split(';'))
+(x2, y2) = map(float, input('Введите ккординаты ВТОРОЙ точки в формате x;y\nКоординаты: ').split(';'))
+
+distance = math.sqrt((x1 - x2) ** 2 + (y1 - y2) ** 2)
+print(f'Расстояние между точками: {distance}')
