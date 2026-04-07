ac76d7a34fcc9979f93befa0c26baa130f94a573
rtxdiv <rtxdiv>
2026-04-07 14:31:20 +0300

    prac4 upd

--- prac4.py
+++ prac4.py
@@ -1,8 +1,31 @@
 import math
 
 
-(x1, y1) = map(float, input('Введите ккординаты ПЕРВОЙ точки в формате x;y\nКоординаты: ').split(';'))
-(x2, y2) = map(float, input('Введите ккординаты ВТОРОЙ точки в формате x;y\nКоординаты: ').split(';'))
+BASH_BOLD = '\033[1m'
+BASH_DEFAULT = '\033[0m'
 
-distance = math.sqrt((x1 - x2) ** 2 + (y1 - y2) ** 2)
-print(f'Расстояние между точками: {distance:.2f} ед.')
+
+def calc_distance(point1: tuple[int | float, int | float], point2: tuple[int | float, int | float]) -> float:
+    """
+    Вычисление расстояния между двумя точками
+
+    :param point1: Кортеж координат первой точки в у.е.
+    :param point2: Кортеж координат второй точки в у.е.
+    :return: None
+    """
+
+    distance: float = math.sqrt((point1[0] - point2[0]) ** 2 + (point1[1] - point2[1]) ** 2)
+    return distance
+
+def main(*args, **kwargs) -> None:
+
+    # Ввод координат
+    (x1, y1) = map(float, input('Введите координаты ПЕРВОЙ точки в формате x;y\nКоординаты: ').split(';'))
+    (x2, y2) = map(float, input('Введите координаты ВТОРОЙ точки в формате x;y\nКоординаты: ').split(';'))
+
+    # Получение дистанции
+    distance = calc_distance((x1, y1), (x2, y2))
+    print(f'{BASH_BOLD}Расстояние между точками: {distance:.2f} у.е.{BASH_DEFAULT}')
+
+
+main()
